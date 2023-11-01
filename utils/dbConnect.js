0133f95a5d7c98bae8185db27cb8b76bd329b32c
pronazmul <developernazmul@gmail.com>
2023-11-01 10:22:50 +0600

    code updated

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,31 +1,30 @@
 // utils/dbConnect.js
 
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
-const uri =
-  'mongodb+srv://vercel-admin-user:CaDeTlEhCaR@cluster0.xn7xrvf.mongodb.net/?retryWrites=true&w=majority' // Replace with your MongoDB connection string.
-const options = {}
+const uri = process.env.MONGODB_URI; // Replace with your MongoDB connection string.
+const options = {
 
-let client
-let clientPromise
+};
 
-if (!url) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
+let client;
+let clientPromise;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
 if (process.env.NODE_ENV === 'development') {
   if (!client) {
-    client = new MongoClient(uri, options)
-    client.connect()
+    client = new MongoClient(uri, options);
+    client.connect();
   }
-  clientPromise = client
+  clientPromise = client;
 } else {
   if (!clientPromise) {
-    client = new MongoClient(uri, options)
-    clientPromise = client.connect()
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
   }
 }
 
-export default clientPromise
+export default clientPromise;
